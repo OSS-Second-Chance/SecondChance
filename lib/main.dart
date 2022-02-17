53e9105120f38bc0a2b2a3a5b788a87621f5e7f0
Will Moffat <wdmoffat@att.net>
2022-02-17 03:21:09 -0500

    Basic dashboard with a list of hardcoded locations. Login and auth stuff is commented out because it wasn't working for me

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const DashboardScreen(title: "SecondChance"),
-      routes: {
-        'dashboard': (context) => const DashboardScreen(title: "SecondChance"),
-      },
+      //routes: {
+        //'dashboard': (context) => const DashboardScreen(title: "SecondChance"),
+     // },
     );
   }
 }
@@ -56,22 +56,14 @@ class MyHomePageState extends State<DashboardScreen> {
   int counter = 0;
   AmplifyState amplifyState = AmplifyState();
   String userButton = "Sign Out";
+  /*
   @override
   initState() {
     super.initState();
     amplifyState.configureAmplify(context, amplifyState, this);
   }
 
-  void incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      counter++;
-    });
-  }
+   */
 
   void setUserState() {
     setState(() {
@@ -86,6 +78,28 @@ class MyHomePageState extends State<DashboardScreen> {
     });
   }
 
+
+  Widget _buildLocations() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 20,
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+
+        final index = i ~/ 2;
+        return _buildRow("Location" + (index + 1).toString());
+      },
+    );
+  }
+
+  Widget _buildRow(String title) {
+    return ListTile(
+        title: Text(title)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -98,6 +112,8 @@ class MyHomePageState extends State<DashboardScreen> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        title: const Text('Second Chance')
+        /*
         title: Text(widget.title),
         actions: <Widget>[
           Padding(
@@ -114,42 +130,9 @@ class MyHomePageState extends State<DashboardScreen> {
             child: Text(userButton)
           )
           )],
+         */
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildLocations(),
     );
   }
 }
